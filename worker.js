ffc21b31381fcb7ab60249f2d24dcaf3f55af020
jmttoki <jmttoki@users.noreply.github.com>
2026-05-18 10:53:13 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -17,7 +17,7 @@ const TEMPLATES = {
         ghPath: "_worker.js",
         defaultVars: ["UUID", "PROXYIP", "DOH", "PATH", "URL", "KEY", "ADMIN"],
         uuidField: "UUID",
-        description: "CMliu (beta2.0) - 建议开启 KV"
+        description: "CMliu (beta2.1) - 建议开启 KV"
     },
     'joey': {
         name: "Joey - 少年你相信光吗",
